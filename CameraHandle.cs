1896a7925b8de9dae5e59c46c4720b524fcc3cbb
windsice <windsice@hotmail.com>
2015-11-30 02:01:09 -1000

    Modified Zooming on the edge, used to not allowing user to zoom out when it detects out of bound, but now it will zoom out but with a shift away from the out of bound side

--- CameraHandle.cs
+++ CameraHandle.cs
@@ -5,7 +5,8 @@ public class CameraHandle : MonoBehaviour {
 	
 	public GameObject background;
 	public float sensitivity = 3f;
-	public float MinZoom = 3f;
+	public float MinZoom = 2.7f;
+	public float MaxZoom = 6.5f;
 
 	private float scroll;
 
@@ -15,51 +16,78 @@ public class CameraHandle : MonoBehaviour {
 	
 	private float CameraScaleY;
 	private float CameraScaleX;
-	private float rightBound;
-	private float leftBound;
-	private float topBound;
-	private float bottomBound;
+	private float BGrightBound,CameraRightBound;
+	private float BGleftBound,CameraLeftBound;
+	private float BGtopBound,CameraTopBound;
+	private float BGbottomBound,CameraBottomBound;
 
 	void Start() {
 
-		leftBound = background.GetComponent<SpriteRenderer>().bounds.center.x - background.GetComponent<SpriteRenderer>().bounds.size.x / 2.0f;
-		rightBound = background.GetComponent<SpriteRenderer>().bounds.center.x + background.GetComponent<SpriteRenderer>().bounds.size.x / 2.0f;
-		bottomBound = background.GetComponent<SpriteRenderer>().bounds.center.y - background.GetComponent<SpriteRenderer>().bounds.size.y / 2.0f;
-		topBound = background.GetComponent<SpriteRenderer>().bounds.center.y + background.GetComponent<SpriteRenderer>().bounds.size.y / 2.0f;
+		BGleftBound = background.GetComponent<SpriteRenderer>().bounds.center.x - background.GetComponent<SpriteRenderer>().bounds.size.x / 2.0f;
+		BGrightBound = background.GetComponent<SpriteRenderer>().bounds.center.x + background.GetComponent<SpriteRenderer>().bounds.size.x / 2.0f;
+		BGbottomBound = background.GetComponent<SpriteRenderer>().bounds.center.y - background.GetComponent<SpriteRenderer>().bounds.size.y / 2.0f;
+		BGtopBound = background.GetComponent<SpriteRenderer>().bounds.center.y + background.GetComponent<SpriteRenderer>().bounds.size.y / 2.0f;
 
 		CameraScaleY = 1.0f;
 		CameraScaleX = CameraScaleY * Camera.main.aspect;
 	
-		Debug.Log (leftBound);
-		Debug.Log (rightBound);
-		Debug.Log (bottomBound);
-		Debug.Log (topBound);
 	}
 	
 	//Correct for view out of boundary
-	bool OutOfBound(){
-		if (Camera.main.orthographicSize * CameraScaleX + transform.position.x > Mathf.Abs(rightBound))
-			return true;
-		if (Camera.main.orthographicSize * CameraScaleX - transform.position.x > Mathf.Abs(leftBound))
-			return true;
-		if (Camera.main.orthographicSize * CameraScaleY + transform.position.y > Mathf.Abs(topBound))
-			return true;
-		if (Camera.main.orthographicSize * CameraScaleY - transform.position.y > Mathf.Abs(bottomBound))
-			return true;
-		return false;
+	//when not CorrectingBound, function simply tell now is out of bound of not.
+	//	otherwise, tell and correct.
+	bool CorrectBound(bool CorrectingBound){
+		bool OutOfBound = false;
+		CameraRightBound = Camera.main.orthographicSize * CameraScaleX + transform.position.x;
+		CameraLeftBound = Camera.main.orthographicSize * CameraScaleX - transform.position.x;
+		CameraTopBound = Camera.main.orthographicSize * CameraScaleY + transform.position.y;
+		CameraBottomBound = Camera.main.orthographicSize * CameraScaleY - transform.position.y;
+
+		if ( CameraRightBound > Mathf.Abs(BGrightBound)){
+			if(CorrectingBound)
+				transform.position = new Vector3(
+					transform.position.x - (CameraRightBound - Mathf.Abs(BGrightBound)),
+					transform.position.y,transform.position.z);
+			OutOfBound =  true;
+		}
+		if (CameraLeftBound > Mathf.Abs (BGleftBound)) {
+			if(CorrectingBound)
+				transform.position = new Vector3(
+					transform.position.x + (CameraLeftBound - Mathf.Abs(BGleftBound)),
+					transform.position.y,transform.position.z);
+			OutOfBound =  true;
+		}
+		if (CameraTopBound > Mathf.Abs (BGtopBound)) {
+			if(CorrectingBound)
+				transform.position = new Vector3(
+					transform.position.x,
+					transform.position.y - (CameraTopBound - Mathf.Abs(BGtopBound)),
+					transform.position.z);
+			OutOfBound =  true;
+		}
+		if (CameraBottomBound > Mathf.Abs (BGbottomBound)) {			
+			if(CorrectingBound)
+			transform.position = new Vector3(
+				transform.position.x,
+					transform.position.y + (CameraBottomBound - Mathf.Abs(BGbottomBound)),
+				transform.position.z);
+			OutOfBound =  true;
+		}
+		return OutOfBound;
 	}
 
 	// Update is called once per frame
 	void Update () {
 
 		//Zooming
-		float scroll = Input.GetAxis ("Mouse ScrollWheel");
+		scroll = Input.GetAxis ("Mouse ScrollWheel") * sensitivity;
 		if (scroll != 0 ) {
-			Camera.main.orthographicSize -= scroll * sensitivity;
-			if(OutOfBound())
-				Camera.main.orthographicSize += scroll * sensitivity;
-			else if(Camera.main.orthographicSize < MinZoom)
+			if(Camera.main.orthographicSize < MinZoom)
 				Camera.main.orthographicSize = MinZoom;
+			else if(Camera.main.orthographicSize > MaxZoom)
+				Camera.main.orthographicSize = MaxZoom;
+			Camera.main.orthographicSize -= scroll;
+			CorrectBound(true);
 		}
 
 		//Translating by Pressing and Releasing ScrollWheel
@@ -72,7 +100,7 @@ public class CameraHandle : MonoBehaviour {
 			newX = transform.position.x + Input.GetAxis("Mouse X");
 			newY = transform.position.y + Input.GetAxis("Mouse Y");
 			transform.localPosition = new Vector3(newX,newY,transform.position.z);
-			if(OutOfBound())
+			if(CorrectBound(false))
 				transform.localPosition = originalPosition;
 		}
 
